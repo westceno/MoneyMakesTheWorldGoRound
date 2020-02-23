f5494e70675f5df3f32e1688aa9c3bead980c2d2
Weston Rainer <Rainerw1@mymail.nku.edu>
2020-02-23 07:11:10 -0500

    text works on camera

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ChangeMoneyText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMoneyText : MonoBehaviour
+{
+
+    public Text myText = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if(PlayerPrefs.HasKey("MoneyAmount"))
+        {
+            myText.text = PlayerPrefs.GetString("MoneyAmount");
+        }
+        else
+        {
+            myText.text = "70,000";
+        }
+    }
+
+
+}
