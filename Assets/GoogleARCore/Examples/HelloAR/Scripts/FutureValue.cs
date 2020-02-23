580c1307d4e433bb5880a510cffe7a481a437406
Weston Rainer <Rainerw1@mymail.nku.edu>
2020-02-23 07:45:39 -0500

    Working version

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/FutureValue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FutureValue : MonoBehaviour
+{
+    public InputField numyears;
+    public InputField amt;
+
+    public void LoadAr()
+    {
+        //PlayerPrefs.SetInt("NumYears", Int32.Parse(numyears.text));
+        PlayerPrefs.SetInt("Count", 20);
+        amt.text = numyears.text;
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private int _frequency;
+
+    public void SelectedFrequency(int index)
+    {
+
+    }
+
+    public void update_numyears(String input_text)
+    {
+    }
+}
